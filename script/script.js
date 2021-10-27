18f3fd57114e40dc4bdab54d03bd453b0019f112
tahsinkoc <tahsinkoc6@gmail.com>
2021-10-27 19:14:12 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -14,33 +14,24 @@ fetch('/script/questions.json')
         let stackBoxParent = document.getElementById('stackBox')
         let answerLetter = document.getElementsByClassName('answer-letter-box')
         let stackBR = document.getElementsByClassName('stack-box')
-
         totalQuestion.textContent = questions.length
-
         questionViewer(queueNumber);
         answerLetterBoxCreator(queueNumber);
-
         checkResult()
-
         goNext.addEventListener('click', function () {
-
             if (queueNumber >= questions.length - 1) {
                 alert('Bütün Bulmacaları Gördünüz')
             }
             else {
-
-
                 for (let j = -1; j < questions[queueNumber].answer.length; j++) {
                     if (answerLetter.length > 0) {
                         answerLetter[0].remove()
                     }
-
                 }
                 for (let geti = 0; geti < arr.length; geti++) {
                     if (stackBR.length > 0) {
                         stackBR[0].remove()
                     }
-
                 }
                 randomLetters = [];
                 arr = [];
@@ -50,22 +41,15 @@ fetch('/script/questions.json')
                 checkResult()
             }
         })
-
         function questionViewer(index) {
             questionC.textContent = questions[index].question
             inQuestion.textContent = index + 1
         }
         function answerLetterBoxCreator(index) {
-
             for (let t = 0; t < 5; t++) {
                 let letter = Math.floor(Math.random() * questions[index].answer.length)
                 randomLetters.push(alphabet[letter])
-
-
             }
-
-
-
             for (let i = 0; i < questions[index].answer.length; i++) {
                 let answerLetterBox = document.createElement('div')
 
@@ -80,50 +64,30 @@ fetch('/script/questions.json')
                     arr.push(randomLetters[rndmNumber])
                     randomLetters.splice(rndmNumber, 1)
                 }
-
-
-
             }
-
             for (let arg = 0; arg < arr.length; arg++) {
                 let stackBox = document.createElement('div')
-
                 stackBox.className = 'stack-box'
                 stackBox.id = arr[arg]
                 stackBox.textContent = arr[arg]
                 // stackBox.setAttribute('onclick', 'checkResult(this.id)')
                 stackBoxParent.appendChild(stackBox)
-
             }
         }
-
         function checkResult() {
             for (let chk = 0; chk < stackBR.length; chk++) {
-
                 stackBR[chk].addEventListener('click', function () {
-
                     let idKepper = stackBR[chk].id
-
                     for (let thk = 0; thk < answerLetter.length; thk++) {
                         if (idKepper == answerLetter[thk].id) {
-
-
                             answerLetter[thk].setAttribute('class', 'answer-letter-box true')
                             answerLetter[thk].textContent = answerLetter[thk].id
 
                             stackBR[chk].setAttribute('class', 'stack-box true')
                             quesControl++
-
-
                         }
-
-
                     }
-
                 })
-
             }
-
         }
-
     })
